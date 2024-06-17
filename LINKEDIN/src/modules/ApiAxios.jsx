19177f331d8703ed55720be69d6362039485c846
Luca-Gerace <geraceluca91@gmail.com>
2024-06-17 21:29:29 +0200

    init progetto

--- LINKEDIN/src/modules/ApiAxios.jsx
+++ LINKEDIN/src/modules/ApiAxios.jsx
@@ -1,9 +1,9 @@
+/* eslint-disable react-refresh/only-export-components */
 import axios from "axios";
-
-let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjcwNzk5MzJiNGYxNDAwMTViMzc0NjYiLCJpYXQiOjE3MTg2NDcxODcsImV4cCI6MTcxOTg1Njc4N30.JjJvYc8A_zVMnDjQEPzkfEeqqsU2wK_6SI3y0GMEk-w'
 
 export default axios.create({
     baseURL: `https://striveschool-api.herokuapp.com/api`,
-    headers: {'Authorization': `Bearer ${token}`}
+    // eslint-disable-next-line no-undef
+    headers: {'Authorization': `Bearer ${process.env.STRIVESCHOOL_API_KEY}`}
   });
   
--- LINKEDIN/vite.config.js
+++ LINKEDIN/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/* eslint-disable no-undef */
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
 
-// https://vitejs.dev/config/
+dotenv.config()
+
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+  },
+  define: {
+    'process.env': process.env
+  }
+});
