786168cd545d5455950f3a21ebdf68a384a615fe
Luca-Gerace <geraceluca91@gmail.com>
2024-06-19 20:01:08 +0200

    some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,30 @@ import UserPage from './pages/UserPage'
 import Error from './pages/Error'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Experiences from './components/Experiences'
-import Sidebar from './components/Sidebar'
-import Profile from './components/Profile'
-import ProfileInfo from './components/ProfileInfo'
 
 export default function App() {
+
+
+  // eslint-disable-next-line no-undef
+  const userId = process.env.USER_ID;
+
+  // Hooks
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${userId}`)
+        .then(response => setUser(response.data))
+        .catch(error => console.error("Error fetching users:", error));
+  }, []);
 
   return (
     <Router>
       <Header />
       <Routes>
-        <Route path='/' element={<Home />}/>
+        <Route path='/' element={<Home user={user} />}/>
         <Route path='/user/:id' element={<UserPage />}/>
         <Route path='*' element={<Error />}/>
       </Routes>
-      <Experiences />
       <Footer /> 
     </Router>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,11 @@
 import Profile from "../components/Profile";
 import Sidebar from "../components/Sidebar";
-import { useEffect, useState } from 'react'
-import axios from '../modules/ApiAxios'
 import Experiences from "../components/Experiences";
 
-export default function Home() {
-
-  // eslint-disable-next-line no-undef
-  const userId = process.env.USER_ID;
-
-  // Hooks
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${userId}`)
-        .then(response => setUser(response.data))
-        .catch(error => console.error("Error fetching users:", error));
-  }, []);
+export default function Home({ user }) {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">HOME LINKEDIN</h1>
       <Profile user={user} />
       <Sidebar />
       <Experiences user={user} />
