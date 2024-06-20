4dc275c4e0c92562460db68bd03a636d75672bf8
“Dema10 <gabrieledematteis@me.com>
2024-06-20 03:19:31 +0200

    Profile completo

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,12 +1,12 @@
-import CameraIcon from '@heroicons/react/24/outline/CameraIcon';
+import { CameraIcon, PencilIcon } from '@heroicons/react/24/outline';
 import ImgUser from '../assets/Profile/IMG_4893ee.jpg';
 import { IconButton } from "@material-tailwind/react";
 import { Button } from "@material-tailwind/react";
 
 export default function Profile({ user }) {
   return (
-    <div className='w-full h-[350px] border border-black'>
-        <div className='relative h-[150px] border bg-[#0d67bc] border-black'>
+    <div className='w-full h-[350px]'>
+        <div className='relative h-[150px] bg-[#0d67bc]'>
             <div className='absolute border-4 top-[50px] left-[10px] border-white rounded-full w-[151px] h-[151px]'>
                 <img src={user.image} alt="image-user" className="rounded-full w-[150px]" />
             </div>
@@ -15,10 +15,15 @@ export default function Profile({ user }) {
                     <CameraIcon className='w-[30px] h-[30px]' style={{ fill: '#6B9DFF' }}/>
                 </IconButton>                
             </div>
+            <div className="absolute right-[15px] bottom-[-50px] w-[40px] h-[40px] bg-[white] flex items-center justify-center gap-4 ">
+                <IconButton className='border-0' variant='outlined'>
+                    <PencilIcon  className='w-[30px] h-[30px]' />
+                </IconButton>                
+            </div>
         </div>
-        <div className='relative h-[200px] border border-black'>
+        <div className='bg-white h-[200px]'>
             <div className='flex'>
-                <div className='mt-[60px] ms-[15px] float-start'>
+                <div className='mt-[55px] ms-[15px] me-[10px]'>
                     <h1 className='text-2xl'>{user.name} {user.surname}</h1>
                     <h6 className='text-lg'>{user.title}</h6>
                     <div className='flex'>
@@ -31,9 +36,6 @@ export default function Profile({ user }) {
                         <Button className='rounded-full me-[4px] p-[7px]' variant='outlined' color="blue">Migliora profilo</Button>
                         <Button className='rounded-full p-[7px]' variant='outlined'>Altro</Button>
                     </div>
-                </div>
-                <div className='float-end'>
-
                 </div>
             </div>
         </div>
