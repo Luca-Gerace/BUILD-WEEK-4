c0cd32de8af1d44289df8a6fa7e7e2cdab34feaf
Luca-Gerace <geraceluca91@gmail.com>
2024-07-22 20:22:05 +0200

    BW5: add express mongoose dotenv + models and routes folders

---
+++ backend/models/User.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose';
+
+// Subschema
+const qualificationSchema = new mongoose.Schema(
+    {
+      name: { type: String, required: true },
+      institute: { type: String, required: true },
+    },
+    {
+      timestamps: true,
+      _id: true,
+    },
+);
+
+const experienceSchema = new mongoose.Schema(
+    {
+        company: { type: String, required: true },
+        role: { type: String, required: true },
+        description: { type: String },
+        logo: { type: String },
+        startDate: { type: Date, required: true },
+        endDate: { type: Date },
+    },
+    {
+        timestamp: true,
+        _id: true,
+    }
+);
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        surname: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        age: { type: Number, required: true },
+        currentPosition: { 
+            enum:['employed', 'unemployed'], 
+            default: 'unemployed', 
+            required: true 
+        },
+        age: { type: Number, required: true },
+        avatar: { type: String },
+        qualifications: [qualificationSchema],
+        qualifications: [experienceSchema]
+    },
+    {
+        timestamp: true,
+        collection: 'users'
+    }
+);
+
+export default mongoose.model('Users', userSchema);
---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import User from '../models/User.js';
+
+// inizializzo il router
+const router = express.Router();
+
+// GET User
+// router.get('/user', async )
